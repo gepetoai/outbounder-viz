924266f2b351d91c61af89e1b49e5d43c9f8ea3a
Wenzo <rithellyenzo@gmail.com>
2025-11-10 17:58:10 -0300

    prevent scrolling on input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,6 +3,12 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+  const handleWheel = (e: React.WheelEvent<HTMLInputElement>) => {
+    if (type === 'number') {
+      e.currentTarget.blur()
+    }
+  }
+
   return (
     <input
       type={type}
@@ -13,6 +19,7 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
         className
       )}
+      onWheel={handleWheel}
       {...props}
     />
   )
